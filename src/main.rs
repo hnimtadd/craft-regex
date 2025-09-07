bc8f60b2d82b0cbe554983deda02a7aa1d944cf5
Craftznake <minhdat15012002@gmail.com>
2025-09-08 03:06:10 +0700

    craft: Search a single-line file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
+use std::fs::File;
 use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::process;
 
 mod regex;
@@ -10,28 +13,37 @@ fn main() {
         println!("Expected first argument to be '-E'");
         process::exit(1);
     }
-
     let pattern = env::args().nth(2).unwrap();
-    let mut input_line = String::new();
-
-    io::stdin().read_line(&mut input_line).unwrap();
-
     let nfa = regex::compiler::compile(&pattern);
 
-    let result = nfa.simulate(&input_line);
-    if result.is_match {
-        println!("{:?}", result.capture_groups);
-        // for cg in result.capture_groups {
-        //     println!(
-        //         "group: {}, from: {}, to: {}, match: {}",
-        //         cg.idx,
-        //         cg.from,
-        //         cg.to,
-        //         &input_line[cg.from..cg.to],
-        //     );
-        // }
-        process::exit(0);
+    if let Some(path) = env::args().nth(3) {
+        let f = File::open(path.as_str()).unwrap();
+        let r = BufReader::new(f);
+        for line_result in r.lines() {
+            let line = line_result.unwrap(); // Check each line for an I/O error
+            let result = nfa.simulate(&line);
+            if result.is_match {
+                println!("{}", line);
+            }
+        }
     } else {
-        process::exit(1);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let result = nfa.simulate(&input);
+        if result.is_match {
+            println!("{:?}", result.capture_groups);
+            // for cg in result.capture_groups {
+            //     println!(
+            //         "group: {}, from: {}, to: {}, match: {}",
+            //         cg.idx,
+            //         cg.from,
+            //         cg.to,
+            //         &input_line[cg.from..cg.to],
+            //     );
+            // }
+            process::exit(0);
+        } else {
+            process::exit(1);
+        }
     }
 }
