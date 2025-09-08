dcede3097e7fe2fc6c43c3820278d5b70aab038d
Craftznake <minhdat15012002@gmail.com>
2025-09-09 01:22:23 +0700

    craft: Search multiple files, rework cli args handling

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,28 @@ use std::process;
 mod logger;
 mod regex;
 
+fn process_file(path: &str, nfa: &regex::compiler::NFA, print_path: bool) -> io::Result<bool> {
+    let f = File::open(path)?;
+
+    let reader = BufReader::new(f);
+
+    let mut is_match = false;
+    for line_result in reader.lines() {
+        let line = line_result?; // Check each line for an I/O error
+        let result = nfa.simulate(&line);
+        if result.is_match {
+            if print_path {
+                println!("{}:{}", path, line);
+            } else {
+                println!("{}", line);
+            }
+            if is_match == false {
+                is_match = true;
+            }
+        }
+    }
+    Ok(is_match)
+}
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
     let is_debug = if let Result::Ok(val) = env::var("debug") {
@@ -17,50 +39,47 @@ fn main() {
         false
     };
     logger::logger::Logger::init(is_debug);
-    if env::args().nth(1).unwrap() != "-E" {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 || args[1] != "-E" {
         log::error!("Expected first argument to be '-E'");
         process::exit(1);
     }
-    let pattern = env::args().nth(2).unwrap();
-    let nfa = regex::compiler::compile(&pattern);
 
-    if let Some(path) = env::args().nth(3) {
-        let f = File::open(path.as_str()).unwrap();
-        let r = BufReader::new(f);
-        let mut is_match = false;
-        for line_result in r.lines() {
-            let line = line_result.unwrap(); // Check each line for an I/O error
-            let result = nfa.simulate(&line);
-            if result.is_match {
-                println!("{}", line);
-                if is_match == false {
-                    is_match = true;
-                }
-            }
-        }
-        if is_match {
-            process::exit(0);
-        } else {
-            process::exit(1);
-        }
-    } else {
+    let pattern = &args[2];
+    let nfa = regex::compiler::compile(&pattern);
+    let paths = args.iter().skip(3).collect::<Vec<&String>>();
+    let mut exit_code = 1;
+
+    if paths.is_empty() {
+        // handle only std in case.
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let result = nfa.simulate(&input);
         if result.is_match {
-            log::info!("{:?}", result.capture_groups);
-            // for cg in result.capture_groups {
-            //     println!(
-            //         "group: {}, from: {}, to: {}, match: {}",
-            //         cg.idx,
-            //         cg.from,
-            //         cg.to,
-            //         &input_line[cg.from..cg.to],
-            //     );
-            // }
-            process::exit(0);
-        } else {
-            process::exit(1);
+            log::debug!("{:?}", result.capture_groups);
+            exit_code = 0;
+        }
+    } else {
+        let mut is_match = false;
+        let print_path = paths.len() > 1;
+        for path in paths {
+            match process_file(path, &nfa, print_path) {
+                Ok(matched) => {
+                    if matched {
+                        is_match = true;
+                        exit_code = 0;
+                    }
+                }
+                Err(e) => {
+                    log::error!("Failed to process file {}: {}", path, e);
+                    exit_code = 1;
+                }
+            }
+        }
+        if is_match {
+            exit_code = 0;
         }
     }
+
+    process::exit(exit_code);
 }
